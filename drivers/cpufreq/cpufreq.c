91df0ab24bd7ec7912481d606d96b7a6a945b9bd
Jackie as a huh? <Mohammadiqbalramadhan21@gmail.com>
2024-07-05 20:50:08 +0000

    Revert "cpufreq: Kill userspace CPU boosting entirely"
    This reverts commit 957545574a3f5ac6c39225de5eaead9d7359c120.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -741,14 +741,7 @@ static ssize_t store_##file_name					\
 	unsigned long val;						\
 	int ret;							\
 									\
-	if (&policy->object == &policy->min)				\
-		return count;						\
-									\
-	memcpy(&new_policy, policy, sizeof(*policy));			\
-	new_policy.min = policy->user_policy.min;			\
-	new_policy.max = policy->user_policy.max;			\
-									\
-	ret = sscanf(buf, "%u", &new_policy.object);			\
+	ret = sscanf(buf, "%lu", &val);					\
 	if (ret != 1)							\
 		return -EINVAL;						\
 									\
